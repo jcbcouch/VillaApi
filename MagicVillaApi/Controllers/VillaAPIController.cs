bab1fe5c8da82807dd971a4aa4cabe03f94022bf
jcbcouch <jcbcouch@gmail.com>
2025-03-15 17:28:50 -0500

    villa model and dto

---
+++ MagicVillaApi/Controllers/VillaAPIController.cs
@@ -0,0 +1,21 @@
+﻿using MagicVillaApi.Models;
+using MagicVillaApi.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVillaApi.Controllers
+{
+    //[Route("api/[controller]")]
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO> {
+            new VillaDTO{Id=1,Name="Pool View" },
+            new VillaDTO{Id=2,Name="Beach View" }
+            };
+        }
+    }
+}
---
+++ MagicVillaApi/Models/Villa.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVillaApi.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
