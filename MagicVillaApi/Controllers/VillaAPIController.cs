3807e1073cd4dae50e44aa8a4585e876ebf33d9a
jcbcouch <jcbcouch@gmail.com>
2025-03-15 21:48:15 -0500

    validation

--- MagicVillaApi/Controllers/VillaAPIController.cs
+++ MagicVillaApi/Controllers/VillaAPIController.cs
@@ -43,6 +43,16 @@ namespace MagicVillaApi.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
         {
+            //if (!ModelState.IsValid)
+            //{
+            //    return BadRequest(ModelState);
+            //}
+            if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null)
+            {
+                ModelState.AddModelError("CustomError", "Villa already Exists!");
+                return BadRequest(ModelState);
+            }
+
             if (villaDTO == null)
             {
                 return BadRequest(villaDTO);
--- MagicVillaApi/Models/Dto/VillaDTO.cs
+++ MagicVillaApi/Models/Dto/VillaDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace MagicVillaApi.Models.Dto
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVillaApi.Models.Dto
 {
     public class VillaDTO
     {
         public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
         public string Name { get; set; }
     }
 }
