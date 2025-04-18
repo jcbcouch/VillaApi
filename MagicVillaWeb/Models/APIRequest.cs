753424b8f0d2b5b0ad72127d6c79c218c3dfe01a
jcbcouch <jcbcouch@gmail.com>
2025-04-18 14:48:25 -0500

    web project models

---
+++ MagicVillaWeb/Models/APIRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.AccessControl;
+using static MagicVillaUtility.SD;
+
+namespace MagicVillaWeb.Models
+{
+    public class APIRequest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        public string Url { get; set; }
+        public object Data { get; set; }
+    }
+}
