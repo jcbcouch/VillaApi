d2eea85e9c8f49dbaa04996b32e396f6acf0ec3a
jcbcouch <jcbcouch@gmail.com>
2025-04-18 15:25:21 -0500

    base api service

---
+++ MagicVillaWeb/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using MagicVillaWeb.Models;
+
+namespace MagicVillaWeb.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
