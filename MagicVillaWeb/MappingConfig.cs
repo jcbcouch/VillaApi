6abb07fed32b7f05be7687ce21594253686d9591
jcbcouch <jcbcouch@gmail.com>
2025-04-18 14:58:00 -0500

    setup automapper in mvc project

---
+++ MagicVillaWeb/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MagicVillaWeb.Models.Dto;
+
+namespace MagicVillaWeb
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
