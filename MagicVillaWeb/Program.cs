f3f1f78a4217e9d0528c52c4bc2583a75f48ce24
jcbcouch <jcbcouch@gmail.com>
2025-04-18 15:52:21 -0500

    villa service

--- MagicVillaWeb/Program.cs
+++ MagicVillaWeb/Program.cs
@@ -1,10 +1,16 @@
 using MagicVillaWeb;
+using MagicVillaWeb.Services.IServices;
+using MagicVillaWeb.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+
+builder.Services.AddHttpClient<IVillaService, VillaService>();
+builder.Services.AddScoped<IVillaService, VillaService>();
 
 var app = builder.Build();
 
