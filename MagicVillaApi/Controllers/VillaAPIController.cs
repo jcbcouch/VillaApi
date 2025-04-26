7b05052d69fd2a4bbf1d23b1821555ce48d32e8b
jcbcouch <jcbcouch@gmail.com>
2025-04-25 20:38:13 -0500

    base service update for validation

--- MagicVillaApi/Controllers/VillaAPIController.cs
+++ MagicVillaApi/Controllers/VillaAPIController.cs
@@ -97,7 +97,7 @@ namespace MagicVillaApi.Controllers
                 //}
                 if (await _dbVilla.GetAsync(u => u.Name.ToLower() == createDTO.Name.ToLower()) != null)
                 {
-                    ModelState.AddModelError("CustomError", "Villa already Exists!");
+                    ModelState.AddModelError("ErrorMessages", "Villa already Exists!");
                     return BadRequest(ModelState);
                 }
 
