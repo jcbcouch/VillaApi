089f8c3d0a1a72a6d9d21c09fb50734e8be9bfce
jcbcouch <jcbcouch@gmail.com>
2025-04-28 13:32:26 -0500

    added sweet alert notifications

--- MagicVillaWeb/Controllers/VillaController.cs
+++ MagicVillaWeb/Controllers/VillaController.cs
@@ -44,9 +44,11 @@ namespace MagicVillaWeb.Controllers
                 var response = await _villaService.CreateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
+                    TempData["success"] = "Villa created successfully";
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["error"] = "Error encountered.";
             return View(model);
         }
 
@@ -55,6 +57,7 @@ namespace MagicVillaWeb.Controllers
             var response = await _villaService.GetAsync<APIResponse>(villaId);
             if (response != null && response.IsSuccess)
             {
+
                 VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
                 return View(_mapper.Map<VillaUpdateDTO>(model));
             }
@@ -67,13 +70,14 @@ namespace MagicVillaWeb.Controllers
         {
             if (ModelState.IsValid)
             {
-
+                TempData["success"] = "Villa updated successfully";
                 var response = await _villaService.UpdateAsync<APIResponse>(model);
                 if (response != null && response.IsSuccess)
                 {
                     return RedirectToAction(nameof(IndexVilla));
                 }
             }
+            TempData["error"] = "Error encountered.";
             return View(model);
         }
 
@@ -96,9 +100,10 @@ namespace MagicVillaWeb.Controllers
             var response = await _villaService.DeleteAsync<APIResponse>(model.Id);
             if (response != null && response.IsSuccess)
             {
+                TempData["success"] = "Villa deleted successfully";
                 return RedirectToAction(nameof(IndexVilla));
             }
-
+            TempData["error"] = "Error encountered.";
             return View(model);
         }
 
