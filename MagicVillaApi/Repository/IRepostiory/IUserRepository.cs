201c490bd8df71aad7cc1cc6d8d6a3fb5dfcbe9b
jcbcouch <jcbcouch@gmail.com>
2025-04-28 13:56:20 -0500

    user repository

---
+++ MagicVillaApi/Repository/IRepostiory/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using MagicVillaApi.Models.Dto;
+using MagicVillaApi.Models;
+
+namespace MagicVillaApi.Repository.IRepostiory
+{
+    public interface IUserRepository
+    {
+        bool IsUniqueUser(string username);
+        Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO);
+        Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO);
+    }
+}
---
+++ MagicVillaApi/Repository/UserRepository.cs
@@ -0,0 +1,32 @@
+﻿using MagicVillaApi.Data;
+using MagicVillaApi.Models.Dto;
+using MagicVillaApi.Models;
+using MagicVillaApi.Repository.IRepostiory;
+
+namespace MagicVillaApi.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public UserRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool IsUniqueUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LoginResponseDTO> Login(LoginRequestDTO loginRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<LocalUser> Register(RegisterationRequestDTO registerationRequestDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
