d8b274ea5e7f6439d51e6c0335a2e73bb7caa9d4
jcbcouch <jcbcouch@gmail.com>
2025-06-17 13:47:47 -0500

    navbar login register logout

--- MagicVillaWeb/Program.cs
+++ MagicVillaWeb/Program.cs
@@ -15,6 +15,8 @@ builder.Services.AddScoped<IVillaService, VillaService>();
 builder.Services.AddHttpClient<IVillaNumberService, VillaNumberService>();
 builder.Services.AddScoped<IVillaNumberService, VillaNumberService>();
 
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddDistributedMemoryCache();
