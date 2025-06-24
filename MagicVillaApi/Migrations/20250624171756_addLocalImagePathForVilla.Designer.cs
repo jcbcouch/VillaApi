b7c0d22b1ec4663dab4c2eb8e0665ebbce97c048
jcbcouch <jcbcouch@gmail.com>
2025-06-24 12:32:27 -0500

    added fields for image upload on villa model and transfer models

--- MagicVillaApi/Migrations/20250624171756_addLocalImagePathForVilla.Designer.cs
+++ MagicVillaApi/Migrations/20250624171756_addLocalImagePathForVilla.Designer.cs
@@ -4,6 +4,7 @@ using MagicVillaApi.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MagicVillaApi.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250624171756_addLocalImagePathForVilla")]
+    partial class addLocalImagePathForVilla
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MagicVillaApi/Migrations/20250624171756_addLocalImagePathForVilla.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MagicVillaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class addLocalImagePathForVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageLocalPath",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedDate", "ImageLocalPath" },
+                values: new object[] { new DateTime(2025, 6, 24, 12, 17, 56, 233, DateTimeKind.Local).AddTicks(9663), null });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedDate", "ImageLocalPath" },
+                values: new object[] { new DateTime(2025, 6, 24, 12, 17, 56, 233, DateTimeKind.Local).AddTicks(9712), null });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "CreatedDate", "ImageLocalPath" },
+                values: new object[] { new DateTime(2025, 6, 24, 12, 17, 56, 233, DateTimeKind.Local).AddTicks(9715), null });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "CreatedDate", "ImageLocalPath" },
+                values: new object[] { new DateTime(2025, 6, 24, 12, 17, 56, 233, DateTimeKind.Local).AddTicks(9717), null });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "CreatedDate", "ImageLocalPath" },
+                values: new object[] { new DateTime(2025, 6, 24, 12, 17, 56, 233, DateTimeKind.Local).AddTicks(9719), null });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageLocalPath",
+                table: "Villas");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2025, 6, 22, 16, 19, 39, 926, DateTimeKind.Local).AddTicks(556));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2025, 6, 22, 16, 19, 39, 926, DateTimeKind.Local).AddTicks(607));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedDate",
+                value: new DateTime(2025, 6, 22, 16, 19, 39, 926, DateTimeKind.Local).AddTicks(609));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedDate",
+                value: new DateTime(2025, 6, 22, 16, 19, 39, 926, DateTimeKind.Local).AddTicks(612));
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "CreatedDate",
+                value: new DateTime(2025, 6, 22, 16, 19, 39, 926, DateTimeKind.Local).AddTicks(614));
+        }
+    }
+}
