d0a822b48cb444a1fd6958a0f045ec6d0356b471
jcbcouch <jcbcouch@gmail.com>
2025-06-27 11:22:12 -0500

    implement token provider

--- MagicVillaWeb/Services/TokenProvider.cs
+++ MagicVillaWeb/Services/TokenProvider.cs
@@ -1,23 +1,45 @@
-﻿using MagicVillaWeb.Models.Dto;
+﻿using MagicVillaUtility;
+using MagicVillaWeb.Models.Dto;
 using MagicVillaWeb.Services.IServices;
 
 namespace MagicVillaWeb.Services
 {
     public class TokenProvider : ITokenProvider
     {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TokenProvider(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor;
+        }
+
+
         public void ClearToken()
         {
-            throw new NotImplementedException();
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.AccessToken);
         }
 
         public TokenDTO GetToken()
         {
-            throw new NotImplementedException();
+            try
+            {
+                bool hasAccessToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.AccessToken, out string accessToken);
+                TokenDTO tokenDTO = new()
+                {
+                    Token = accessToken
+                };
+                return hasAccessToken ? tokenDTO : null;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
         }
 
         public void SetToken(TokenDTO tokenDTO)
         {
-            throw new NotImplementedException();
+            var cookieOptions = new CookieOptions { Expires = DateTime.UtcNow.AddDays(60) };
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.AccessToken, tokenDTO.Token, cookieOptions);
         }
     }
 }
