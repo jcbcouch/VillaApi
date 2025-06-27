34aaba2d5f9125aed8c8c5d3ddd4605a588c3bb0
jcbcouch <jcbcouch@gmail.com>
2025-06-27 11:01:47 -0500

    token provider service

---
+++ MagicVillaWeb/Services/IServices/ITokenProvider.cs
@@ -0,0 +1,11 @@
+﻿using MagicVillaWeb.Models.Dto;
+
+namespace MagicVillaWeb.Services.IServices
+{
+    public interface ITokenProvider
+    {
+        void SetToken(TokenDTO tokenDTO);
+        TokenDTO? GetToken();
+        void ClearToken();
+    }
+}
